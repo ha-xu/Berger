410ae53b43fbfbf728de23d0ccee4e59d228919d
alizee <alizee.xia@universite-paris-saclay.fr>
2024-03-04 16:33:44 +0100

    ajout de boutton

--- src/view/GameUIPanel.java
+++ src/view/GameUIPanel.java
@@ -9,6 +9,24 @@ public class GameUIPanel extends JPanel {
         this.setLayout(null);
         this.setVisible(true);
 
+        JPanel sous_panel = new JPanel();
+        sous_panel.setBounds(0, 50, GameFrame.WIDTH-GameFrame.HEIGHT, GameFrame.HEIGHT/2);
+        sous_panel.setBackground(Color.LIGHT_GRAY);
+        this.add(sous_panel);
+
+        JButton button_sheep = new JButton("BUY SHEEP");
+        button_sheep.setBounds(0, 50, 150, 15);
+        sous_panel.add(button_sheep);
+
+        JLabel label_coin = new JLabel("number of coins");
+        label_coin.setBounds(GameFrame.WIDTH/2, 0, 150, 15);
+        this.add(label_coin);
+
+
+
+
+
+
         setBackground(Color.BLUE);
     }
 }
